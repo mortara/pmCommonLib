be72f04ebab64c32367f1e82584fd28a5d1f8398
Patrick Mortara <mortara@gmail.com>
2025-02-24 15:20:21 +0100

    ...

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#include "../OTA/ota_handler.h"
+#include "../WiFi/wifimanager.hpp"
+#include "../MQTT/mqtt.hpp"
+#include "../Webserial/webserial.hpp"
+#include "../Webserver/webserver.hpp"
+
+void setup() 
+{
+ 
+}
+
+
+void loop() {
+  
+ 
+}

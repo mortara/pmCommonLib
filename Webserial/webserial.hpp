293479f4ce776b2d05cb39669c5c3e3f0a39ee82
Patrick Mortara <mortara@gmail.com>
2025-02-24 12:47:17 +0100

    ...

--- Webserial/webserial.hpp
+++ Webserial/webserial.hpp
@@ -2,10 +2,8 @@
 #include <Arduino.h>
 #if defined(ESP8266)
 #include "ESP8266WiFi.h"
-#include "ESPAsyncTCP.h"
 #elif defined(ESP32)
 #include "WiFi.h"
-#include "AsyncTCP.h"
 #endif
 
 #include "ESPAsyncWebServer.h"

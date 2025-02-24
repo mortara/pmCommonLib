01dc31df77e0e155ad8fe6aaee52e61c9d78d4fb
Patrick Mortara <mortara@gmail.com>
2025-02-24 12:28:46 +0100

    Fixes for ESP8266

--- Wifi/wifimanager.hpp
+++ Wifi/wifimanager.hpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
-#include <WiFi.h>
+#if defined(ESP8266)
+#include "ESP8266WiFi.h"
+#elif defined(ESP32)
+#include "WiFi.h"
+#endif
 #include "../Webserial/webserial.hpp"
 
 #ifndef WIFIMANAGER_H

b21042bdcf15d874548d68616be19b3888ee936f
Patrick Mortara <mortara@gmail.com>
2025-03-03 22:30:38 +0100

    Added a Wifi Manager to the project

--- src/Wifi/wifimanager.cpp
+++ src/Wifi/wifimanager.cpp
@@ -1,55 +1,187 @@
 #include "wifimanager.hpp"
 #include "../MQTT/mqtt.hpp"
 
-void WIFIManagerClass::Setup(String hostname, String SSID, String PASS)
-{
-    WiFi.disconnect();
+// Initialize LittleFS
+void WIFIManagerClass::initLittleFS() {
+    if (!LittleFS.begin(true)) {
+      Serial.println("An error has occurred while mounting LittleFS");
+    }
+    Serial.println("LittleFS mounted successfully");
+  }
+  
+  // Read File from LittleFS
+  String readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\r\n", path);
+  
+    File file = fs.open(path);
+    if(!file || file.isDirectory()){
+      Serial.println("- failed to open file for reading");
+      return String();
+    }
+    
+    String fileContent;
+    while(file.available()){
+      fileContent = file.readStringUntil('\n');
+      break;     
+    }
+    return fileContent;
+  }
+  
+  // Write file to LittleFS
+  void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\r\n", path);
+  
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+      Serial.println("- failed to open file for writing");
+      return;
+    }
+    if(file.print(message)){
+      Serial.println("- file written");
+    } else {
+      Serial.println("- write failed");
+    }
+  }
 
-    _credentials.SSID = SSID;
-    _credentials.PASS = PASS;
-
+  bool WIFIManagerClass::initWiFi() {
+    if(_credentials.SSID =="" || _credentials.IP==""){
+      Serial.println("Undefined SSID or IP address.");
+      return false;
+    }
+  
     WiFi.mode(WIFI_STA);
-    WiFi.setSleep(true);
-    WiFi.setAutoReconnect(true);
-    //Connect();
+    localIP.fromString(_credentials.IP.c_str());
+    localGateway.fromString(_credentials.Gateway.c_str());
+  
+    IPAddress subnet(255, 255, 255, 0);
+    if (!WiFi.config(localIP, localGateway, subnet)){
+      Serial.println("STA Failed to configure");
+      return false;
+    }
+    WiFi.begin(_credentials.SSID.c_str(), _credentials.PASS.c_str());
+    Serial.println("Connecting to WiFi...");
+  
+    unsigned long currentMillis = millis();
+    previousMillis = currentMillis;
+  
+    while(WiFi.status() != WL_CONNECTED) {
+      currentMillis = millis();
+      if (currentMillis - previousMillis >= interval) {
+        Serial.println("Failed to connect.");
+        return false;
+      }
+    }
+  
+    Serial.println(WiFi.localIP());
+    return true;
+  }
 
-    WiFi.setHostname(hostname.c_str());
+  String WIFIManagerClass::processor(const String& var) {
+    if(var == "STATE") {
+     
+    }
+    return String();
+  }
+
+void ServeWIFILogonPage(AsyncWebServerRequest *request)
+{
+    int params = request->params();
+    for(int i=0;i<params;i++){
+        const AsyncWebParameter* p = request->getParam(i);
+        if(p->isPost()){
+        // HTTP POST ssid value
+        if (p->name() == String(PARAM_INPUT_1)) {
+            _credentials.SSID = p->value().c_str();
+            Serial.print("SSID set to: ");
+            Serial.println(_credentials.SSID);
+            // Write file to save value
+            writeFile(LittleFS, ssidPath, _credentials.SSID.c_str());
+        }
+        // HTTP POST pass value
+        if (p->name() == String(PARAM_INPUT_2)) {
+            _credentials.PASS = p->value().c_str();
+            Serial.print("Password set to: ");
+            Serial.println(_credentials.PASS);
+            // Write file to save value
+            writeFile(LittleFS, passPath, _credentials.PASS.c_str());
+        }
+        // HTTP POST ip value
+        if (p->name() == String(PARAM_INPUT_3)) {
+            _credentials.IP = p->value().c_str();
+            Serial.print("IP Address set to: ");
+            Serial.println(_credentials.IP);
+            // Write file to save value
+            writeFile(LittleFS, ipPath, _credentials.IP.c_str());
+        }
+        // HTTP POST gateway value
+        if (p->name() == String(PARAM_INPUT_4)) {
+            _credentials.Gateway = p->value().c_str();
+            Serial.print("Gateway set to: ");
+            Serial.println(_credentials.Gateway);
+            // Write file to save value
+            writeFile(LittleFS, gatewayPath, _credentials.Gateway.c_str());
+        }
+        //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+        }
+    }
+    request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + _credentials.IP);
+    delay(3000);
+    ESP.restart();
+}
+
+void WIFIManagerClass::Setup(String hostname, AsyncWebServer *server)
+{
+    // Serial port for debugging purposes
+    Serial.begin(115200);
+
+    initLittleFS();
+
+    // Load values saved in LittleFS
+    _credentials.SSID = readFile(LittleFS, ssidPath);
+    _credentials.PASS = readFile(LittleFS, passPath);
+    _credentials.IP = readFile(LittleFS, ipPath);
+    _credentials.Gateway = readFile (LittleFS, gatewayPath);
+    Serial.println(_credentials.SSID);
+    Serial.println(_credentials.PASS);
+    Serial.println(_credentials.IP);
+    Serial.println(_credentials.Gateway);
+
+    if(initWiFi()) 
+    {
+ 
+    }
+    else 
+    {
+        // Connect to Wi-Fi network with SSID and password
+        Serial.println("Setting AP (Access Point)");
+        // NULL sets an open Access Point
+        WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+
+        IPAddress IP = WiFi.softAPIP();
+        Serial.print("AP IP address: ");
+        Serial.println(IP); 
+
+        // Web Server Root URL
+        server->on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(LittleFS, "/wifimanager.html", "text/html");
+        });
+        
+        server->serveStatic("/", LittleFS, "/");
+        
+        server->on("/", HTTP_POST, ServeWIFILogonPage);
+
+    }
 }
 
 bool WIFIManagerClass::Connect(String SSID, String PASS)
 {
-    _credentials.SSID = SSID;
-    _credentials.PASS = PASS;
-
+   
     return Connect();
 }
 
 bool WIFIManagerClass::Connect()
 {
-    if(WiFi.status() == WL_CONNECTED || connecting)
-    {
-        //WebSerialLogger.println("Already connected or connecting");
-        return true;
-    }
-    
-    connecting = true;
-    WiFi.disconnect();
-    WiFi.begin(_credentials.SSID.c_str(), _credentials.PASS.c_str());
-    WebSerialLogger.println("Connecting to WiFi ..");
-    _lastConnectionTry = millis();
-    /*int timeout = 10;
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print('.');
-        delay(1000);
-        timeout--;
-        if(timeout == 0)
-        {
-            Serial.println("WIFI timeout ......");
-            return false;
-        }
-    }
-    connected = true;
-    DisplayInfo();*/
+   
     return true;
 }
 
@@ -162,4 +294,5 @@ void WIFIManagerClass::Loop()
     }
 }
 
+WIFICreds _credentials;
 WIFIManagerClass WIFIManager;
--- src/Wifi/wifimanager.hpp
+++ src/Wifi/wifimanager.hpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #if defined(ESP8266)
-#include "ESP8266WiFi.h"
+#include <ESPAsyncTCP.h>
+#include <ESP8266WiFi.h>
 #elif defined(ESP32)
-#include "WiFi.h"
+#include <AsyncTCP.h>
+#include <WiFi.h>
 #endif
 #include "../Webserial/webserial.hpp"
+#include "LittleFS.h"
 
 #ifndef WIFIMANAGER_H
 #define WIFIMANAGER_H
@@ -12,24 +15,55 @@
 typedef struct {
     String SSID;  //stream url
     String PASS; //stations name
+    String IP;
+    String Gateway;
 } WIFICreds;
+
+extern WIFICreds _credentials;
+
+#define PARAM_INPUT_1 "ssid"
+#define PARAM_INPUT_2 "pass"
+#define PARAM_INPUT_3 "ip"
+#define PARAM_INPUT_4 "gateway"
+#define ssidPath "/ssid.txt"
+#define passPath "/pass.txt"
+#define ipPath "/ip.txt"
+#define gatewayPath "/gateway.txt"
+
 
 class WIFIManagerClass
 {
     private:
+        
         bool connecting = false;
         bool connected = false;
-        WIFICreds _credentials;
-        unsigned long interval = 15000;
+     
+
+        unsigned long interval = 10000;
         unsigned long _lastConnectionTry = 0;
         unsigned long _lastMqttupdate = 0;
-        
+
+        // Timer variables
+        unsigned long previousMillis = 0;
+      
         void setupMQTT();
         bool mqttsetup = false;
 
+        
+        IPAddress localIP;
+        //IPAddress localIP(192, 168, 1, 200); // hardcoded
+        String processor(const String& var);
+        // Set your Gateway IP address
+        IPAddress localGateway;
+        //IPAddress localGateway(192, 168, 1, 1); //hardcoded
+        
+        void initLittleFS();
+       
+        bool initWiFi();
+
     public:
 
-        void Setup(String hostname, String SSID, String PASS);
+        void Setup(String hostname, AsyncWebServer *webserver);
         bool Connect();
         bool Connect(String SSID, String PASS);
         void Disconnect();
